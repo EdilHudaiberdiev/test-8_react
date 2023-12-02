35bf60a2f901531fd27df7d0c5e22be3c42ee202
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2023-12-02 15:01:22 +0600

    added AddQuotes and Home components

--- src/Containers/AddQuotes/AddQuotes.tsx
+++ src/Containers/AddQuotes/AddQuotes.tsx
@@ -1,9 +1,83 @@
+import {useNavigate} from "react-router-dom";
+import React, {useState} from "react";
+import axiosApi from "../../axiosApi";
+
+interface Props {
+  text: string,
+  author: string,
+  category: string,
+}
 
 const AddQuotes = () => {
+  const Navigation = useNavigate();
+  const categories = [
+    {title: 'Star Wars', id: 'star-wars'},
+    {title: 'Motivational', id: 'motivational'},
+    {title: 'Humour', id: 'humour'},
+    {title: 'Famous people', id: 'famous-people'},
+    {title: 'Harry Potter', id: 'harry-potter'},
+  ];
+  const [quote, setQuote] = useState<Props>({
+    text: '',
+    author: '',
+    category: '',
+  });
+
+
+  const changeForm = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    setQuote((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await axiosApi.post('quotes.json', quote);
+    } finally {
+      Navigation('/');
+    }
+  };
+
   return (
-    <div>
-      AddQuotes
-    </div>
+    <>
+      <form onSubmit={onFormSubmit}>
+        <h2 className="text-center mb-4">Add new quote</h2>
+        <div className="mb-3 w-75 mx-auto">
+          <label htmlFor="author" className="form-label">Author</label>
+          <input
+            type="author"
+            name="author"
+            id="author"
+            className="form-control"
+            value={quote.author}
+            onChange={changeForm}
+          />
+        </div>
+
+        <select name="category" onChange={changeForm}>
+          {categories.map(category => (
+            <option key={category.id} value={category.id}>{category.title}</option>
+          ))}
+        </select>
+
+        <div className="mb-3 w-75 mx-auto">
+          <label htmlFor="text" className="form-label">Quote</label>
+          <textarea
+            name="text"
+            id="text"
+            className="form-control"
+            value={quote.text}
+            onChange={changeForm}
+          ></textarea>
+        </div>
+        <div className="text-center">
+          <button type="submit" className="btn btn-primary">Send</button>
+        </div>
+      </form>
+    </>
   );
 };
 
--- src/Containers/Home/Home.tsx
+++ src/Containers/Home/Home.tsx
@@ -1,10 +1,68 @@
+import {useEffect, useState} from "react";
+import axiosApi from "../../axiosApi";
+import {NavLink, useParams} from "react-router-dom";
+
+export interface IQuotes {
+  id: string;
+  author: string;
+  text: string;
+}
 
 const Home = () => {
+  const params = useParams();
+  const [quotes, setQuotes] = useState<IQuotes[]>([]);
+  const categories = [
+    {title: 'Star Wars', id: 'star-wars'},
+    {title: 'Motivational', id: 'motivational'},
+    {title: 'Humour', id: 'humour'},
+    {title: 'Famous people', id: 'famous-people'},
+    {title: 'Harry Potter', id: 'harry-potter'},
+  ];
+
+  const fetchData = async (url: string) => {
+
+    const quotesArray = [];
+    const response = await axiosApi.get(url);
+    const quotes: {[key: string]: IQuotes} = response.data;
+    console.log(response)
+
+    if (response.data !== null) {
+      for (const [key, value] of Object.entries(quotes)) {
+        quotesArray.push({
+          id: key,
+          author: value.author,
+          text: value.text,
+        });
+      }
+      setQuotes(quotesArray);
+    }
+
+  };
+
+  useEffect(() => {
+
+    if (params.category === undefined) {
+      fetchData('/quotes.json').catch(e => console.error(e));
+    } else {
+      fetchData(`/quotes.json?orderBy="category"&equalTo="${params.category}"`).catch(e => console.error(e));
+    }
+
+  }, [params.category]);
+
+
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <div>
+        <NavLink to={`/quotes/`}>All</NavLink>
+      </div>
+      {categories.map(category => (
+        <div key={category.id}>
+          <NavLink to={`/quotes/${category.id}`}>{category.title}</NavLink>
+        </div>
+      ))}
+    </>
   );
 };
+
 
 export default Home;
